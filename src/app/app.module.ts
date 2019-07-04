a2e738986acd8630fa4be5e068e368fae604b1e0
alex-krugliak <a.krugliak@aimprosoft.com>
2019-07-04 17:52:57 +0300

    First setup commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,48 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ConfigModule } from '@spartacus/core';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule, defaultCmsContentConfig } from '@spartacus/storefront';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    B2cStorefrontModule.withConfig({
+      authentication: {
+        client_id: 'mobile_android',
+        client_secret: 'secret'
+      },
+      backend: {
+        occ: {
+          baseUrl: 'https://localhost:9002',
+          prefix: '/rest/v2/',
+          legacy: false
+        }
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      context: {
+        urlEncodingParameters: ['baseSite', 'language', 'currency'],
+        parameters: {
+          baseSite: {
+            values: [
+              'electronics-spa'
+            ],
+            persistence: 'route',
+          },
+        },
+      },
+
+    }),
+    ConfigModule.withConfigFactory(defaultCmsContentConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
