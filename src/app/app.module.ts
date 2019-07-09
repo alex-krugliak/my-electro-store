2072cf39e91d6cf183bf70916265df061793ecb5
alex-krugliak <a.krugliak@aimprosoft.com>
2019-07-09 16:30:16 +0300

    setup config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 
 import { ConfigModule } from '@spartacus/core';
 import { translations, translationChunksConfig } from '@spartacus/assets';
-import { B2cStorefrontModule, defaultCmsContentConfig } from '@spartacus/storefront';
+import { B2cStorefrontModule, defaultCmsContentConfig, b2cLayoutConfig } from '@spartacus/storefront';
+import {LayoutSlotConfig} from '@spartacus/storefront/layout/config/layout-config';
 
 @NgModule({
   declarations: [
@@ -17,14 +18,14 @@ import { B2cStorefrontModule, defaultCmsContentConfig } from '@spartacus/storefr
     AppRoutingModule,
     B2cStorefrontModule.withConfig({
       authentication: {
-        client_id: 'mobile_android',
+        client_id: 'client4kyma',
         client_secret: 'secret'
       },
       backend: {
         occ: {
           baseUrl: 'https://localhost:9002',
           prefix: '/rest/v2/',
-          legacy: false
+          legacy: true
         }
       },
       i18n: {
@@ -43,7 +44,7 @@ import { B2cStorefrontModule, defaultCmsContentConfig } from '@spartacus/storefr
           },
         },
       },
-
+      layoutSlots: b2cLayoutConfig.layoutSlots
     }),
     ConfigModule.withConfigFactory(defaultCmsContentConfig)
   ],
