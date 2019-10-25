c357cd440b5f1bf8c1dcccc28e0dc877b69fcad2
alex-krugliak <a.krugliak@aimprosoft.com>
2019-10-25 15:08:42 +0300

    Add product comparing page

--- src/app/customize/cms/product-comparing/product-comparing/product-comparing.component.ts
+++ src/app/customize/cms/product-comparing/product-comparing/product-comparing.component.ts
@@ -1,4 +1,11 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {filter, map, switchMap} from 'rxjs/operators';
+import {ProductComparingService} from '../../../core/comparisons/store/facade/product-comparing.service';
+import {ProductComparing} from '../../../core/comparisons/store/comparisons.state';
+import {Observable} from 'rxjs';
+import * as _ from 'lodash';
+import {isDefined} from '../../../utils/common.utils';
 
 @Component({
   selector: 'cx-product-comparing',
@@ -7,12 +14,28 @@ import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@an
 })
 export class ProductComparingComponent implements OnInit {
 
+  comparingCategory$: Observable<ProductComparing>;
+
   constructor(
+    private route: ActivatedRoute,
     private cd: ChangeDetectorRef,
+    private productComparingService: ProductComparingService
   ) {
   }
 
   ngOnInit(): void {
+    debugger
+
+    this.comparingCategory$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        return this.productComparingService.getProductComparingData().pipe(
+          filter(categoryComparison => isDefined(categoryComparison)),
+          map((categoryComparison: ProductComparing[]) => {
+            return _.find(categoryComparison, productComparing => productComparing.categoryCode === params.get('id'));
+          })
+        );
+      }),
+    );
   }
 
 
