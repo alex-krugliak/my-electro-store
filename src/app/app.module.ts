fe4743923dac028f072a0f7e0690b75a474eda5d
alex <s_krug@ukr.net>
2022-04-20 17:54:08 +0300

    Update spartacus to version 4.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,98 +1,25 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-
-import {ConfigModule} from '@spartacus/core';
-import {translationChunksConfig, translations} from '@spartacus/assets';
-import {B2cStorefrontModule, CmsPageGuard, defaultCmsContentConfig, PageLayoutComponent} from '@spartacus/storefront';
-import {SpartacusProductListModule} from './customize/cms/product-list/spartacus-product-list.module';
-import {ComparisonsCoreModule} from './customize/core/comparisons/comparisons.core.module';
-import {RouterModule} from '@angular/router';
-import {customTranslationChunksConfig, customTranslations} from './customize/assets/translations/translations';
-
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SpartacusModule } from './spartacus/spartacus.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    SpartacusProductListModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    ComparisonsCoreModule,
-    RouterModule.forChild([
-      {
-        path: 'comparing-categories',
-        data: {pageLabel: 'category-comparing'},
-        component: PageLayoutComponent,
-        canActivate: [CmsPageGuard]
-      },
-      {
-        // path with dynamic param:
-        path: 'comparing-products/:id',
-        data: {pageLabel: 'product-comparing'},
-        component: PageLayoutComponent,
-        canActivate: [CmsPageGuard]
-      }
-    ]),
-    B2cStorefrontModule.withConfig({
-      authentication: {
-        client_id: 'client4kyma',
-        client_secret: 'secret'
-      },
-      backend: {
-        occ: {
-          baseUrl: 'https://localhost:9012',
-          prefix: '/rest/v2/',
-          legacy: true,
-          endpoints: {
-            comparisons: 'users/${userId}/comparisons'
-          }
-        }
-      },
-      i18n: {
-        resources: customTranslations,
-        chunks: customTranslationChunksConfig,
-        fallbackLang: 'en'
-      },
-      context: {
-        urlEncodingParameters: ['baseSite', 'language', 'currency'],
-        baseSite: ['electronics-spa']
-      },
-      layoutSlots: {
-        header: {
-          md: {
-            slots: [
-              'PreHeader',
-              'SiteContext',
-              'SiteLinks',
-              'SiteLogo',
-              'SearchBox',
-              'SiteLogin',
-              'ComparingIcon',
-              'MiniCart',
-              'NavigationBar',
-            ],
-          },
-          xs: {
-            slots: ['PreHeader', 'SiteLogo', 'SearchBox', 'ComparingIcon', 'MiniCart'],
-          },
-        }
-      },
-      icon: {
-        symbols: {
-          COMPARING: 'fas fa-balance-scale fa-2x',
-          CART: 'fas fa-shopping-cart',
-        }
-      }
-
-    }),
-    ConfigModule.withConfigFactory(defaultCmsContentConfig)
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    SpartacusModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
